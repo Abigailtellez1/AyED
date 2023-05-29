5e2b72dae84e524b6194a4894a2e5fd5fd7f5e77
Abigail Tellez <atellezcerna@frba.utn.edu.ar>
2023-05-29 19:12:23 -0300

    Update main.cpp

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -1,1 +1,17 @@
+#include <iostream> 
+#include <iomanip>
+#include <cmath>
 
+using namespace std;
+
+int main(){
+    double pi=0, n=1;
+    for (int i = 0 ; abs(n*4) > 0.0000001; i++)    // hasta que n se considere lo suficientemente pequeño
+    {
+        n= pow(-1,i)/(2*i+1);
+        pi+= n;
+
+    }
+    cout << setprecision(6)<< fixed << pi*4 ;
+
+}
